fc2408f0c3de9aba60ab3252222df0569c0e436a
rochellev <rochellevroberts@utexas.edu>
2019-06-07 09:24:18 -0700

    set up form in index.html

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("form#quiz").submit(function(event) {
+      event.preventDefault();
+      //debugger;
+  
+    //   var number1 = parseInt($("#length1").val());
+    //   var number2 = parseInt($("#length2").val());
+    //   var number3 = parseInt($("#length3").val());
+  
+  
+    });
+  });
