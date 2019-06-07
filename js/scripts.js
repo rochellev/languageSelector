b47672cbb2af71ed3cd98a8d9f68068b73fa89a6
rochellev <rochellevroberts@utexas.edu>
2019-06-07 11:35:14 -0700

    implemented way to get user input in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
-$(document).ready(function() {
-    $("form#quiz").submit(function(event) {
-      event.preventDefault();
-      //debugger;
-  
-    //   var number1 = parseInt($("#length1").val());
-    //   var number2 = parseInt($("#length2").val());
-    //   var number3 = parseInt($("#length3").val());
-  
-  
-    });
+$(document).ready(function () {
+  $("form#quiz").submit(function (event) {
+    event.preventDefault();
+    debugger;
+    // get user input for each question
+    var morning = $("input:radio[name=question1]:checked").val();
+    var catDog = $("input:radio[name=question2]:checked").val();
+    var popularLang = $("input:radio[name=question3]:checked").val();
+    var memory = $("input:radio[name=question4]:checked").val();
+    var epicodus = $("input:radio[name=question5]:checked").val();
+    var gameDev = $("input:radio[name=question6]:checked").val();
+    var threads = $("input:radio[name=question7]:checked").val();
+
   });
+});
